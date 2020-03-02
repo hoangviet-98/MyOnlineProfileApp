62239a52016ad1ef63235f09ef1f8ec74e623387
Hoàng Thế Việt <vietht098@gmail.com>
2020-03-02 21:04:26 +0700

    secondcommit

--- OneDrive/Máy tính/project NTD/MyBlogApplication/src/app/app.component.ts
+++ OneDrive/Máy tính/project NTD/MyBlogApplication/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import {PostService} from './services/post.service';
+import {Post} from './services/Post'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  providers: [PostService],
+  template: `
+  <body>
+  <div class= "container">
+  <h1> Welcome </h1>
+  <ul>
+    <li *ngFor = "let post of posts">
+      <h3> {{post.title}} </h3>
+      <p> {{post.body}}</p>
+  </li>
+  </ul>
+  </div>
+  </body>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyBlogApplication';
+  posts: Post[];
+  title = "Hello"
+ constructor(private _postService : PostService){
+    this._postService.getPosts().then(
+      posts => {this.posts = posts}
+    );
+ }
 }
