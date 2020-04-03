a56170e017b48a72d673a65ae388f5d0fad87ff0
Hoàng Thế Việt <vietht098@gmail.com>
2020-04-03 20:04:30 +0700

    Updates

---
+++ precache-manifest.c6514138f330127992837f830c98a285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1890563d78245861b960fdca77fcd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f5ac5b0ecbb43d20b1",
+    "url": "/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "1f9566625889dcee5f4f",
+    "url": "/static/js/2.78209f54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78209f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f5ac5b0ecbb43d20b1",
+    "url": "/static/js/main.992c24d0.chunk.js"
+  },
+  {
+    "revision": "532e17138c1adeb38255",
+    "url": "/static/js/runtime-main.0e4591b2.js"
+  }
+]);
